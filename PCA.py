c8a07e367ff4f90adcaf424129e6be3c9e6fd47f
quintenmank <quintenmank@hotmail.com>
2020-03-30 13:51:18 +0200

    pca

--- PCA.py
+++ PCA.py
@@ -1,22 +1,14 @@
 '''
 Determine the number of principal components for the PCA
 '''
-#%%
-import pandas as pd
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import LeaveOneOut 
-from sklearn.model_selection import RepeatedKFold
-from sklearn.feature_selection import mutual_info_classif
+# %%
 from sklearn.decomposition import PCA
-from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import MinMaxScaler
-import sklearn.linear_model 
 import matplotlib.pyplot as plt
 import numpy as np
 
-import statistics
-#%%
+
+# %%
 
 from hn.load_data import load_data
 
@@ -24,10 +16,10 @@ data = load_data()
 print(f'The number of samples: {len(data.index)}')
 print(f'The number of columns: {len(data.columns)}')
 
-#%%
+# %%
 filepath = '/Users/quintenmank/Desktop/TM10007/TM10007_PROJECT/TM10007_PROJECT-1/hn/HN_radiomicFeatures.csv'
 data = np.genfromtxt(filepath, delimiter=',', dtype='float64')
-scaler = MinMaxScaler(feature_range=[0,1])
+scaler = MinMaxScaler(feature_range=[0, 1])
 data_rescaled = scaler.fit_transform(data[1:, 1:])
 data_rescaled = np.nan_to_num(data_rescaled)
 

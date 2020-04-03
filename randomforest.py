8b1e2c42f8a5caa88e2143409bab7a8c43c01179
Marie-ClaireD <marieclairedoornbos@gmail.com>
2020-04-03 15:07:27 +0200

    Beste hyperparameters uit search gehaald

--- randomforest.py
+++ randomforest.py
@@ -125,13 +125,14 @@ def random_forest_search(x, y):
     Random Forest Random Search for Hyperparameters
     """
 
-    crss_val = RepeatedKFold(n_splits=5, n_repeats=10, random_state=None)           
+    crss_val = RepeatedKFold(n_splits=5, n_repeats=1, random_state=None)           
     crss_val.get_n_splits(x, y)
 
 #    predict_labels = []
 #    predict_probas = []
 #    y_val_total = []
-
+    
+    accuracy_rs = []
     accuracies = []
     auc_scores = []
     specificities = []
@@ -173,7 +174,8 @@ def random_forest_search(x, y):
 #    predict_probas = np.array(predict_probas)
     #print(predict_labels)
     #print(predict_probas)
-        prediction = random_search.predict(x_val)
+        
+        prediction = random_search.predict(x_val)        
         performance_scores = pd.DataFrame()                    
         auc_scores.append(roc_auc_score(y_val, prediction))
         conf_mat = confusion_matrix(y_val, prediction)
@@ -227,7 +229,92 @@ print(performances)
 #    return predict_labels, predict_probas, y_val_total
 
 # predict_labels_rf_s, predict_proba_rf_s, y_val_total_rf_s = random_forest_search(x_train, y_train)
+#%%
+#%% Random Forest n_estimator search GOEDE
+def random_forest_search(x, y):
+    """ 
+    Random Forest Random Search for Hyperparameters
+    """
 
+    crss_val = RepeatedKFold(n_splits=5, n_repeats=1, random_state=None)           
+    crss_val.get_n_splits(x, y)
+
+#    predict_labels = []
+#    predict_probas = []
+#    y_val_total = []
+    
+    accuracy_rs = []
+    accuracies = []
+    auc_scores = []
+    specificities = []
+    sensitivities = []
+    tprs = []
+    aucs = []
+    base_fpr = np.linspace(0, 1, 101)
+
+    param_dist = {"n_estimators": randint(1, 200),
+                    "max_features": randint(5, 30),
+                    "max_depth": randint(2, 18),
+                    "min_samples_leaf": randint(1, 17)}
+    clf = RandomForestClassifier(bootstrap=True, random_state=None)
+    random_search = RandomizedSearchCV(clf, param_distributions=param_dist, n_iter=5, cv=5, n_jobs=-1) #Hier nog een keer CV?
+    model = random_search.fit(x, y)
+    hyperparameters = model.best_estimator_.get_params()
+    pprint(hyperparameters)
+
+    for train_index, val_index in crss_val.split(x, y):
+        x_train, x_val = x[train_index], x[val_index]
+        y_train, y_val = y[train_index], y[val_index]
+
+        clf = RandomForestClassifier(bootstrap=True, max_depth=hyperparameters.get('max_depth'), max_features=hyperparameters.get('max_features'), min_samples_leaf=hyperparameters.get('min_samples_leaf'), n_estimators=hyperparameters.get('n_estimators'), random_state=None)
+        clf.fit(x_train, y_train)
+        prediction = clf.predict(x_val)        
+        performance_scores = pd.DataFrame()                    
+        auc_scores.append(roc_auc_score(y_val, prediction))
+        conf_mat = confusion_matrix(y_val, prediction)
+        total = sum(sum(conf_mat))
+        accuracies.append((conf_mat[0, 0]+conf_mat[1, 1])/total)
+        sensitivities.append(conf_mat[0, 0]/(conf_mat[0, 0]+conf_mat[0, 1]))
+        specificities.append(conf_mat[1, 1]/(conf_mat[1, 0]+conf_mat[1, 1]))
+        performance_scores['Accuracy'] = accuracies
+        performance_scores['AUC'] = auc_scores
+        performance_scores['Sensitivity'] = sensitivities
+        performance_scores['Specificity'] = specificities
+
+        predicted_probas = random_search.predict_proba(x_val)[:, 1]
+        fpr, tpr, _ = roc_curve(y_val, predicted_probas)
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        tpr = interp(base_fpr, fpr, tpr)
+        tpr[0] = 0.0
+        tprs.append(tpr)
+    
+    tprs = np.array(tprs)
+    mean_tprs = tprs.mean(axis=0)
+    std = tprs.std(axis=0)
+
+    mean_auc = auc(base_fpr, mean_tprs)
+    std_auc = np.std(aucs)
+
+    tprs_upper = np.minimum(mean_tprs + std, 1)
+    tprs_lower = mean_tprs - std
+    plt.figure(figsize=(12, 8))
+    plt.plot(base_fpr, mean_tprs, 'c', alpha=0.8, label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),)
+    plt.fill_between(base_fpr, tprs_lower, tprs_upper, color='c', alpha=0.2)
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='k', label='Chance level', alpha=0.8)
+    plt.xlim([-0.01, 1.01])
+    plt.ylim([-0.01, 1.01])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.legend(loc="lower right")
+    plt.title('Receiver operating characteristic (ROC) curve')
+    plt.grid()
+    plt.show()
+
+    return performance_scores
+
+performances = random_forest_search(x_train, y_train)
+print(performances)
 #%%
   # Hyperparameters: 
     # n_estimators: number of decision trees

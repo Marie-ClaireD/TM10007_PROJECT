e275e4349581d70798f3ceffaf807a2b5c4bb441
Marie-ClaireD <marieclairedoornbos@gmail.com>
2020-04-09 11:57:35 +0200

    Plotjes erbij

--- restje.py
+++ restje.py
@@ -201,9 +201,18 @@ param_distributions = [{'penalty': ['l1', 'l2', 'elasticnet', 'none'],
                         'min_samples_leaf': randint(1, 20)}, {'C': randint(0.1, 100),
                         'gamma': ['auto', 'scale'], 'kernel': ['rbf', 'poly', 'sigmoid', 'linear']}]
 for clf, param_dist in zip(clsfs, param_distributions):
-    performance_scores = pd.DataFrame()
-    models = []
-    accuracy = []
+
+    accuracies = []
+    auc_scores = []
+    specificities = []
+    sensitivities = []
+    tprs = []
+    aucs = []
+    base_fpr = np.linspace(0, 1, 101)
+
+    # performance_scores = pd.DataFrame()
+    # models = []
+    # accuracy = []
     crss_val = RepeatedStratifiedKFold(n_splits=5, n_repeats=1, random_state=None) 
     for train_index, test_index in crss_val.split(features, labels):
         x_train, x_test = features[train_index], features[test_index]
@@ -220,13 +229,80 @@ for clf, param_dist in zip(clsfs, param_distributions):
 
         random_search = RandomizedSearchCV(clf, param_distributions=param_dist, n_iter=5, cv=5, scoring='accuracy', n_jobs=-1)
         model = random_search.fit(x_train, y_train)
-        models.append(model.best_estimator_)
-        accuracy.append(model.best_score_)
+        model = model.best_estimator_
+        #models.append(model)
+        prediction = model.predict(x_test)
 
-    performance_scores['Classifiers'] = models
-    performance_scores['Accuracy'] = accuracy
+        performance_scores = pd.DataFrame()                  
+        auc_scores.append(roc_auc_score(y_test, prediction))
+        conf_mat = confusion_matrix(y_test, prediction)
+        total = sum(sum(conf_mat))
+        accuracies.append((conf_mat[0, 0]+conf_mat[1, 1])/total)
+        sensitivities.append(conf_mat[0, 0]/(conf_mat[0, 0]+conf_mat[0, 1]))
+        specificities.append(conf_mat[1, 1]/(conf_mat[1, 0]+conf_mat[1, 1]))
+        performance_scores['Accuracy'] = accuracies
+        performance_scores['AUC'] = auc_scores
+        performance_scores['Sensitivity'] = sensitivities
+        performance_scores['Specificity'] = specificities
 
-    pprint(performance_scores)
+        predicted_probas = model.predict_proba(x_test)[:, 1]
+        fpr, tpr, _ = roc_curve(y_test, predicted_probas)
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        tpr = interp(base_fpr, fpr, tpr)
+        tpr[0] = 0.0
+        tprs.append(tpr)
+ 
+    tprs = np.array(tprs)
+    mean_tprs = tprs.mean(axis=0)
+    std = tprs.std(axis=0)
+
+    mean_auc = auc(base_fpr, mean_tprs)
+    std_auc = np.std(aucs)
+
+    tprs_upper = np.minimum(mean_tprs + std, 1)
+    tprs_lower = mean_tprs - std
+    plt.figure(figsize=(12, 8))
+    plt.plot(base_fpr, mean_tprs, 'c', alpha=0.8, label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),)
+    plt.fill_between(base_fpr, tprs_lower, tprs_upper, color='c', alpha=0.2)
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='k', label='Chance level', alpha=0.8)
+    plt.xlim([-0.01, 1.01])
+    plt.ylim([-0.01, 1.01])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.legend(loc="lower right")
+    plt.title(f'Receiver operating characteristic (ROC) curve {model}')
+    plt.grid()
+    plt.show()
+
+    performance_clf = []
+    clsfs = [LogisticRegression(), KNeighborsClassifier(leaf_size=hyperparameters[0].get('leaf_size'), n_neighbors=hyperparameters[0].get('n_neighbors'), p=hyperparameters[0].get('p')), RandomForestClassifier(bootstrap=True, max_depth=hyperparameters[1].get('max_depth'), max_features=hyperparameters[1].get('max_features'), min_samples_leaf=hyperparameters[1].get('min_samples_leaf'), n_estimators=hyperparameters[1].get('n_estimators'), random_state=None), SVC(C=hyperparameters[2].get("C"), gamma=hyperparameters[2].get("gamma"), kernel=hyperparameters[2].get("kernel"), probability=True)]
+    clsfs_names =['Logistic Regression', 'kNN', 'Random Forest', 'SVM']
+
+    for clf in clsfs:
+        performances = cross_val_scores(x_train, y_train, hyperparameters, clf_int) 
+        performance_clf.append(performances_int)
+
+    data1 = pd.DataFrame(performance_clf[0], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=1)
+    data2 = pd.DataFrame(performance_clf[1], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=2)
+    data3 = pd.DataFrame(performance_clf[2], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=3)
+    data4 = pd.DataFrame(performance_clf[3], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=4)
+
+    cdf = pd.concat([data1, data2, data3, data4])
+    mdf = pd.melt(cdf, id_vars=['Location'], var_name=['Index'])
+
+    ax = sns.boxplot(x="Location", y="value", hue="Index", data=mdf)    
+    plt.xticks([0, 1, 2, 3], ['Logistic Regression', 'kNN', 'Random Forest', 'SVM'])
+    ax.set_xlabel('Classifier')
+    ax.set_ylabel('Performance')
+    plt.show()
+    
+        # accuracy.append(model.best_score_)
+
+    # performance_scores['Classifiers'] = models
+    # performance_scores['Accuracy'] = accuracy
+
+    # pprint(performance_scores)
 
 
     # logistic_reg.append(models[0])

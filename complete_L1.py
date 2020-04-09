4f44242125006669a428ab095c8d52108a90709d
quintenmank <quintenmank@hotmail.com>
2020-04-09 15:11:41 +0200

    l1 aangepast

--- complete_L1.py
+++ complete_L1.py
@@ -25,7 +25,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-
+import seaborn as sns
 #%%
 from hn.load_data import load_data
 
@@ -111,6 +111,7 @@ hyperparameters = get_hyperparameters(x_train, y_train)
 
 
 #%%
+
 def cross_val_scores_l1(x, y, hyperparameters, clf):
     '''
     Cross validation using a Logistic Regression classifier (5 folds)
@@ -136,11 +137,13 @@ def cross_val_scores_l1(x, y, hyperparameters, clf):
         y_train, y_val= y[train_index], y[val_index]
         scaler = StandardScaler()
         scaler.fit(pd.DataFrame(x_train).fillna(0))
-        lasso = SelectFromModel(estimator=Lasso(alpha=0.0001, random_state=None, max_iter=10000), threshold='median')
+        lasso = SelectFromModel(estimator=Lasso(alpha=0.035, random_state=None, max_iter=10000), threshold='median')
         lasso.fit(scaler.transform(pd.DataFrame(x_train).fillna(0)), y_train) 
         lasso.get_support()
         selected_feat = data1.columns[(lasso.get_support())]        
-        
+        print('total features: {}'.format((x_train.shape[1])))
+        print('features with coefficients shrank to zero: {}'.format(
+     np.sum(lasso.estimator_.coef_ == 0)))
         # Getting a list of removed features
         removed_feats = [(lasso.estimator_.coef_ == 0).ravel().tolist()]
         # print(removed_feats)
@@ -210,3 +213,144 @@ for item in performance_clf:
     plt.figure()
     item.boxplot()
     plt.show()
+
+# %% NEW
+
+
+from sklearn.linear_model import Lasso
+from sklearn.feature_selection import SelectFromModel
+numerics = ['int16','int32','int64','float16','float32','float64']
+numerical_vars = list(data.select_dtypes(include=numerics).columns)
+data1 = data[numerical_vars]
+clsfs = [SVC(probability=True),LogisticRegression(), KNeighborsClassifier(), RandomForestClassifier(bootstrap=True, random_state=None)]
+names = ['Logistic Regression', 'kNN', 'Random Forest', 'SVM']
+param_distributions = [{'C': randint(0.1, 100),
+                        'gamma': ['auto', 'scale'], 'kernel': ['sigmoid', 'linear']},{'penalty': ['l1', 'l2', 'elasticnet', 'none'],
+                        'max_iter': randint(1, 100)}, {'leaf_size': randint(1, 50),
+                        'n_neighbors': randint(1, 20), 'p': [1, 2]}, {'n_estimators': randint(1, 500),
+                        'max_features': randint(1, 30), 'max_depth': randint(1, 20),
+                        'min_samples_leaf': randint(1, 20)}]
+performance_clf = []
+for clf,name, param_dist in zip(clsfs, names, param_distributions):
+    accuracies = []
+    auc_scores = []
+    specificities = []
+    sensitivities = []
+    tprs = []
+    aucs = []
+    base_fpr = np.linspace(0, 1, 101)
+
+    # performance_scores = pd.DataFrame()
+    # models = []
+    # accuracy = []
+    crss_val = RepeatedStratifiedKFold(n_splits=5, n_repeats=1, random_state=None) 
+    for train_index, test_index in crss_val.split(features, labels):
+        x_train, x_test = features[train_index], features[test_index]
+        y_train, y_test= labels[train_index], labels[test_index]
+
+        scaler = StandardScaler()
+        scaler.fit(pd.DataFrame(x_train).fillna(0))
+        lasso = SelectFromModel(estimator=Lasso(alpha=0.035))
+        lasso.fit(scaler.transform(pd.DataFrame(x_train).fillna(0)), y_train) 
+        lasso.get_support()
+        selected_feat = data1.columns[(lasso.get_support())]        
+        print('total features: {}'.format((x_train.shape[1])))
+        print('features with coefficients shrank to zero: {}'.format(np.sum(lasso.estimator_.coef_ == 0)))
+        # Getting a list of removed features
+        removed_feats = [(lasso.estimator_.coef_ == 0).ravel().tolist()]
+
+        # Remove features from training and validation set
+        x_train = lasso.transform(pd.DataFrame(x_train).fillna(0))
+      
+        x_test = lasso.transform(pd.DataFrame(x_test).fillna(0))
+        
+
+        random_search = RandomizedSearchCV(clf, param_distributions=param_dist, n_iter=5, cv=5, scoring='accuracy', n_jobs=-1)
+        model = random_search.fit(x_train, y_train)
+        model = model.best_estimator_
+        #models.append(model)
+        prediction = model.predict(x_test)
+
+        performance_scores = pd.DataFrame()                  
+        auc_scores.append(roc_auc_score(y_test, prediction))
+        conf_mat = confusion_matrix(y_test, prediction)
+        total = sum(sum(conf_mat))
+        accuracies.append((conf_mat[0, 0]+conf_mat[1, 1])/total)
+        sensitivities.append(conf_mat[0, 0]/(conf_mat[0, 0]+conf_mat[0, 1]))
+        specificities.append(conf_mat[1, 1]/(conf_mat[1, 0]+conf_mat[1, 1]))
+        performance_scores['Accuracy'] = accuracies
+        performance_scores['AUC'] = auc_scores
+        performance_scores['Sensitivity'] = sensitivities
+        performance_scores['Specificity'] = specificities
+
+        predicted_probas = model.predict_proba(x_test)[:, 1]
+        fpr, tpr, _ = roc_curve(y_test, predicted_probas)
+        roc_auc = auc(fpr, tpr)
+        aucs.append(roc_auc)
+        tpr = interp(base_fpr, fpr, tpr)
+        tpr[0] = 0.0
+        tprs.append(tpr)
+ 
+    tprs = np.array(tprs)
+    mean_tprs = tprs.mean(axis=0)
+    std = tprs.std(axis=0)
+
+    mean_auc = auc(base_fpr, mean_tprs)
+    std_auc = np.std(aucs)
+
+    tprs_upper = np.minimum(mean_tprs + std, 1)
+    tprs_lower = mean_tprs - std
+    plt.figure(figsize=(12, 8))
+    plt.plot(base_fpr, mean_tprs, 'c', alpha=0.8, label=r'Mean ROC (AUC = %0.2f $\pm$ %0.2f)' % (mean_auc, std_auc),)
+    plt.fill_between(base_fpr, tprs_lower, tprs_upper, color='c', alpha=0.2)
+    plt.plot([0, 1], [0, 1], linestyle='--', lw=2, color='k', label='Chance level', alpha=0.8)
+    plt.xlim([-0.01, 1.01])
+    plt.ylim([-0.01, 1.01])
+    plt.ylabel('True Positive Rate')
+    plt.xlabel('False Positive Rate')
+    plt.legend(loc="lower right")
+    plt.title(f'Receiver operating characteristic (ROC) curve {name}')
+    plt.grid()
+    plt.show()
+
+    performance_clf.append(performance_scores)
+
+data1 = pd.DataFrame(performance_clf[0], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=1)
+data2 = pd.DataFrame(performance_clf[1], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=2)
+data3 = pd.DataFrame(performance_clf[2], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=3)
+data4 = pd.DataFrame(performance_clf[3], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=4)
+
+cdf = pd.concat([data1, data2, data3, data4])
+mdf = pd.melt(cdf, id_vars=['Location'], var_name=['Index'])
+
+
+ax = sns.boxplot(x="Location", y="value", hue="Index", data=mdf)    
+plt.xticks([0, 1, 2, 3], names)
+ax.set_xlabel('Classifier')
+ax.set_ylabel('Performance')
+plt.show()
+
+    # performance_clf = []
+    # clsfs = [LogisticRegression(), KNeighborsClassifier(leaf_size=hyperparameters[0].get('leaf_size'), n_neighbors=hyperparameters[0].get('n_neighbors'), p=hyperparameters[0].get('p')), RandomForestClassifier(bootstrap=True, max_depth=hyperparameters[1].get('max_depth'), max_features=hyperparameters[1].get('max_features'), min_samples_leaf=hyperparameters[1].get('min_samples_leaf'), n_estimators=hyperparameters[1].get('n_estimators'), random_state=None), SVC(C=hyperparameters[2].get("C"), gamma=hyperparameters[2].get("gamma"), kernel=hyperparameters[2].get("kernel"), probability=True)]
+    # clsfs_names =['Logistic Regression', 'kNN', 'Random Forest', 'SVM']
+
+    # for clf in clsfs:
+    #     performances = cross_val_scores(x_train, y_train, hyperparameters, clf_int) 
+    #     performance_clf.append(performances_int)
+
+    # data1 = pd.DataFrame(performance_clf[0], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=1)
+    # data2 = pd.DataFrame(performance_clf[1], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=2)
+    # data3 = pd.DataFrame(performance_clf[2], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=3)
+    # data4 = pd.DataFrame(performance_clf[3], columns=['Accuracy', 'AUC', 'Sensitivity', 'Specificity']).assign(Location=4)
+
+    # cdf = pd.concat([data1, data2, data3, data4])
+    # mdf = pd.melt(cdf, id_vars=['Location'], var_name=['Index'])
+
+    # ax = sns.boxplot(x="Location", y="value", hue="Index", data=mdf)    
+    # plt.xticks([0, 1, 2, 3], ['Logistic Regression', 'kNN', 'Random Forest', 'SVM'])
+    # ax.set_xlabel('Classifier')
+    # ax.set_ylabel('Performance')
+    # plt.show()
+    
+
+# %%
